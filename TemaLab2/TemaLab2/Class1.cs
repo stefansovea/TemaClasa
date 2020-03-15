ddca887575002ad9bdf6bba1080de7b01a4d3cc4
Stefan Sovea <stefansovea@outlook.com>
2020-03-15 21:46:30 +0200

    Varianta initiala

---
+++ TemaLab2/TemaLab2/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaLab2
+{
+    class Automobile
+    {
+        string marca;
+        string culoare;
+        int pret;
+
+        public Automobile()
+        {
+            marca = string.Empty;
+            culoare = string.Empty;
+            pret = 0;
+        }
+        public Automobile(string _marca, string _culoare, int _pret)
+        {
+            marca = _marca;
+            culoare = _culoare;
+            pret = _pret;
+        }
+
+        public string afisare()
+        {
+            return string.Format("Se alege un automobil marca {0}, de culoare {1}, la pretul de {2} lei", marca, culoare, pret);
+        }
+    }
+}
---
+++ TemaLab2/TemaLab2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Configuration;
+
+namespace TemaLab2
+{
+    //    Suma/diferenta a doua numere preluate de la tastatura
+    class Program
+    {
+        static void Main()
+        {
+            Automobile f = new Automobile("Audi", "Rosu", 123231312);
+            string s = f.afisare();
+            Console.WriteLine(s);
+        }
+    }
+}
